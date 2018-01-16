11e92ddc6e5a9257a40a6ac10765239c069d4085
AdrianSzromba <adrian.szromba@gmail.com>
2018-01-16 10:41:19 +0100

    add new filds for tweets

--- src/main/java/pl/coderslab/warsztat6/entity/User.java
+++ src/main/java/pl/coderslab/warsztat6/entity/User.java
@@ -1,10 +1,15 @@
 package pl.coderslab.warsztat6.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -35,6 +40,13 @@ public class User {
 	@Email
 	@Column(unique = true)
 	private String email;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Tweet> tweet = new ArrayList<>();
+	
+	public User() {
+		super();
+	}
 
 	public long getId() {
 		return id;
@@ -79,4 +91,13 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public List<Tweet> getTweet() {
+		return tweet;
+	}
+
+	public void setTweet(List<Tweet> tweet) {
+		this.tweet = tweet;
+	}
+	
 }
