99224bcd5b1fb4fb98b9cb97e97ac723186a685a
AdrianSzromba <adrian.szromba@gmail.com>
2018-01-17 18:41:25 +0100

    add form and mapping to create new tweet

--- src/main/java/pl/coderslab/warsztat6/controller/TweetController.java
+++ src/main/java/pl/coderslab/warsztat6/controller/TweetController.java
@@ -1,9 +1,20 @@
 package pl.coderslab.warsztat6.controller;
 
+import java.util.Date;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pl.coderslab.warsztat6.bean.SessionManager;
+import pl.coderslab.warsztat6.entity.Tweet;
+import pl.coderslab.warsztat6.entity.User;
 import pl.coderslab.warsztat6.repository.TweetRepository;
 
 @Controller
@@ -13,4 +24,17 @@ public class TweetController {
 	@Autowired
 	TweetRepository tweetRepository;
 
+	@PostMapping("/add")
+	public String addPost(@Valid @ModelAttribute Tweet tweet, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "home";
+		}
+		HttpSession s = SessionManager.session();
+		User u = (User)s.getAttribute("user");
+		tweet.setUser(u);
+		tweet.setCreated(new Date());
+		this.tweetRepository.save(tweet);
+		return "redirect:/";
+	}
+	
 }
